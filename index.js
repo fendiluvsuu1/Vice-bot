9c3ee12a24d78cb9acdc2da488de6e870cbb5a1f
fendiluvsuu1 <fendiluvsuu@gmail.com>
2025-02-28 23:35:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["VICE OT!", "🎮 RUNNING TURFS"];
+const statusMessages = [".gg/5mleakz", "BEST 5MLEAKZ 👑];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
